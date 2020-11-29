6894e6710257b4b466328532abf33d9ffc16c6e5
Ricky Pai <rickyp999+github@gmail.com>
2020-11-29 12:35:48 -0800

    tests for existing path helpers

---
+++ table/table_paths_test.go
@@ -0,0 +1,76 @@
+package table
+
+import (
+	"path/filepath"
+	"testing"
+)
+
+func TestTable_SchemaTablesDirPath(t *testing.T) {
+	type fields struct {
+		TableName        string
+		FilenameOverride string
+		ModelClass       string
+	}
+	tests := []struct {
+		name  string
+		table Table
+		want  string
+	}{
+		{
+			"returns schema/tables by default",
+			Table{},
+			filepath.Join("schema", "tables"),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := tt.table.SchemaTablesDirPath(); got != tt.want {
+				t.Errorf("Table.SchemaTablesDirPath() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestTable_SchemaQueriesDirPath(t *testing.T) {
+	tests := []struct {
+		name  string
+		table Table
+		want  string
+	}{
+		{
+			"returns schema/queries by default",
+			Table{},
+			filepath.Join("schema", "queries"),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := tt.table.SchemaQueriesDirPath(); got != tt.want {
+				t.Errorf("Table.SchemaQueriesDirPath() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestTable_DbModelsDirPath(t *testing.T) {
+	tests := []struct {
+		name  string
+		table Table
+		want  string
+	}{
+		{
+			"returns dbmodels/{{table name}} by default",
+			Table{
+				TableName: "hi",
+			},
+			filepath.Join("dbmodels", "hi"),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := tt.table.DbModelsDirPath(); got != tt.want {
+				t.Errorf("Table.DbModelsDirPath() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
